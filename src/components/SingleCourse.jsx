4a6d5a832c01b9bdb2d895626e536a3a522f556d
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-09-12 12:46:48 +0700

    4- added navigation if page is does not exist

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,16 +1,27 @@
 import courses from "../data/courses"
-import { Link, useParams } from "react-router-dom"
+import { Link, useNavigate, useParams, useLocation } from "react-router-dom"
+// import NotFound from "./NotFound"
+import { useEffect } from "react"
 
 const SingleCourse = () => {
   const params = useParams()
+  const navigate=useNavigate()
+  const location = useLocation()
+  console.log(location)
   const course = courses.find((course)=> course.slug === params.slug)
+//   if (!course) { return <NotFound/>}
+useEffect(()=> {
+    if(!course){
+        navigate("..", {relative:'path'})
+    }
+} , [course, navigate])
   return <>
-  <h1>{course.title}</h1>
-  <h2>{course.slug}</h2>
-  <p>{course.id}</p>
+  <h1>{course?.title}</h1>
+  <h2>{course?.slug}</h2>
+  <p>{course?.id}</p>
   <Link to=".." relative="path">All courses</Link>
   </>
 
 }
 
-export default SingleCourse
+export default SingleCourse 
